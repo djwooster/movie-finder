8018d8f5edf21900482048669c20c0c68f6fe46a
DJ Wooster <contact@djwooster.com>
2024-07-20 07:15:12 -1000

    Test Message

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const [practice, usePractice] = useState(null);
 
   const handleSelectMovie = (id) => {
     setSelectedId((previd) => (previd === id ? null : id));
